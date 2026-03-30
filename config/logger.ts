f07f20c7d7234ba5f2537b0baf8dc0385fb7bd10
elkira01 <elkira01@gmail.com>
2026-03-30 17:04:48 +0100

    Update logger env

--- config/logger.ts
+++ config/logger.ts
@@ -16,8 +16,11 @@ const loggerConfig = defineConfig({
       level: env.get('LOG_LEVEL'),
       transport: {
         targets: targets()
-          .pushIf(!app.inProduction, targets.pretty())
-          .pushIf(app.inProduction, targets.file({ destination: 1 }))
+          .pushIf(!app.inProduction && app.nodeEnvironment !== 'staging', targets.pretty())
+          .pushIf(
+            app.inProduction || app.nodeEnvironment === 'staging',
+            targets.file({ destination: 1 })
+          )
           .toArray(),
       },
     },
