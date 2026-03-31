0aded0da4d7d6d90b2023ff06580d4f801c130f7
elkira01 <elkira01@gmail.com>
2026-03-31 05:58:39 +0100

    Set default for address JSON field
    Set default for address JSON field to an empty object.
    Rename two content description migrations to new timestamps:
    - 1742382000000_alter_content_description_to_string.ts
      -> 1770912738098_alter_content_description_to_string.ts
    - 1742382100000_alter_content_description_to_text.ts
      -> 1770912738099_alter_content_description_to_text.ts

--- database/migrations/1774148600947_create_update_store_addresses_table.ts
+++ database/migrations/1774148600947_create_update_store_addresses_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.json('address').notNullable()
+      table.json('address').notNullable().defaultTo(JSON.stringify({}))
     })
   }
 
