7d8970df941139d01b130e3f596ea1e9083ad636
lebus237 <elkira01@gmail.com>
2026-04-15 16:48:29 +0100

    fix(product): correct ProductModifierGroupListItemDto import path in query handlers

--- src/kernel/product/application/query-handler/list_product_modifier_groups_handler.ts
+++ src/kernel/product/application/query-handler/list_product_modifier_groups_handler.ts
@@ -1,6 +1,6 @@
 import { QueryHandler } from '#shared/application/use-cases/query_handler'
 import { PaginatedResultDto } from '#shared/application/collection/paginated_result'
-import { ProductModifierGroupListItemDto } from '#kernel/product/application/dto/product_modifier_read_dto'
+import { ProductModifierGroupListItemDto } from '#kernel/product/application/dto/product_modifier_group_read_dto'
 import { ProductModifierGroupCollection } from '#kernel/product/application/collection/product_modifier_group_collection'
 import { ListProductModifierGroupsQuery } from '#kernel/product/application/query/list_product_modifier_groups_query'
 
